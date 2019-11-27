62875a7e2ffddb8095dd4852c5608657cf11f930
JUN HU <huj@hiscene.com>
2019-11-27 14:01:04 +0800

    Update：使用EBO和VAO

--- app/src/main/cpp/opengl.cpp
+++ app/src/main/cpp/opengl.cpp
@@ -9,12 +9,31 @@
 #include "native-lib.h"
 #include "utils/AssetReader.h"
 
+const char *vShaderPath = "vtriangle.vert";
+const char *fShaderPath = "ftriangle.glsl";
+
+
 //定义顶点数组
 float vertices[] = {
         -0.5f, -0.5f, 0.0f,
         0.5f, -0.5f, 0.0f,
         0.0f, 0.5f, 0.0f
 };
+
+
+//只存储4个点，由索引指定绘制的顺序
+float rectangleVertices[] = {
+        0.5f, 0.5f, 0.0f,   // 右上角
+        0.5f, -0.5f, 0.0f,  // 右下角
+        -0.5f, -0.5f, 0.0f, // 左下角
+        -0.5f, 0.5f, 0.0f   // 左上角
+};
+
+unsigned int indices[] = { // 注意索引从0开始!
+        0, 1, 3, // 第一个三角形
+        1, 2, 3  // 第二个三角形
+};
+
 
 //顶点着色器会在GPU上创建内存，用于存储顶点数据
 //需要配置OpenGL如何解释这些内存，指定如何发送给显卡
@@ -23,6 +42,14 @@ float vertices[] = {
 
 //生成一个VBO对象，用一个id来表示
 unsigned int VBO;
+
+//着色器程序
+uint mProgram;
+
+//生成一个VAO对象
+uint VAO;
+
+uint EBO;
 
 void generateVBO() {
     //生成一个VBO缓冲对象
@@ -41,17 +68,19 @@ void generateVBO() {
     //GL_STATIC_DRAW ：数据不会或几乎不会改变。
     //GL_DYNAMIC_DRAW：数据会被改变很多。
     //GL_STREAM_DRAW ：数据每次绘制时都会改变。
-    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(rectangleVertices), rectangleVertices, GL_STATIC_DRAW);
+
+//    glBindBuffer(GL_ARRAY_BUFFER, 0);
 }
 
 //动态编译顶点着色器源码
 //创建着色器对象，还是用id存储
-unsigned int generateVertexShader() {
+unsigned int generateVertexShader(const char *shader) {
     unsigned int vertexShaderId = 0;
     //创建shader
     vertexShaderId = glCreateShader(GL_VERTEX_SHADER);
 
-    const char *vShader = readFromAsset("vtriangle.vert");
+    const char *vShader = readFromAsset(shader);
     //将着色器代码附着到着色器对象上
     glShaderSource(vertexShaderId, 1, &vShader, nullptr);
 
@@ -70,12 +99,12 @@ unsigned int generateVertexShader() {
 }
 
 //动态编译片段着色器
-unsigned int generateFramgentShader() {
+unsigned int generateFramgentShader(const char *shader) {
     unsigned int fragmentShaderId = 0;
     fragmentShaderId = glCreateShader(GL_FRAGMENT_SHADER);
 
 
-    const char *fShader = readFromAsset("ftriangle.glsl");
+    const char *fShader = readFromAsset(shader);
     glShaderSource(fragmentShaderId, 1, &fShader, nullptr);
     glCompileShader(fragmentShaderId);
 
@@ -115,23 +144,107 @@ unsigned int generateProgram(uint vShader, uint fShader) {
     }
 }
 
-int linkProgram() {
-    auto vShader = generateVertexShader();
+uint linkProgram() {
+    auto vShader = generateVertexShader(vShaderPath);
     if (vShader == 0) {
         LOGD("opengl", "linkProgram 顶点着色器生成失败");
-        return -1;
+        return 0;
     }
-    auto fShader = generateFramgentShader();
+    auto fShader = generateFramgentShader(fShaderPath);
     if (fShader == 0) {
         LOGD("opengl", "linkProgram 片元着色器生成失败");
-        return -1;
+        return 0;
     }
     auto program = generateProgram(vShader, fShader);
     if (program == 0) {
         LOGD("opengl", "linkProgram 链接失败");
-        return -1;
+        return 0;
     }
+
+    //激活程序对象，这样以后渲染都是调用这个着色器程序了
+    glUseProgram(program);
+
+    //删除
+    glDeleteShader(vShader);
+    glDeleteShader(fShader);
+
     return program;
+}
+
+
+//由于顶点着色器允许指定各种形式的输入，所有我们需要让OpenGL知道如何读取这些数据
+//例如我们当前的VBO是顶点数据以XYZ形式连续排列，所以顶点连续排列
+void handleVBO() {
+    //告诉OpenGL如何读取VBO数据
+    //各个参数说明
+//
+//    第一个参数指定从索引0开始取数据，与顶点着色器中layout(location=0)对应。
+//
+//    第二个参数指定顶点属性大小，顶点属性是vec3，则由3个值xyz组成
+//
+//    第三个参数指定数据类型。
+//
+//    第四个参数定义是否希望数据被标准化（归一化），只表示方向不表示大小。
+//
+//    第五个参数是步长（Stride），指定在连续的顶点属性之间的间隔。
+//
+//    第六个参数表示我们的位置数据在缓冲区起始位置的偏移量。
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), nullptr);
+    //以上面的索引值0作为参数，启动顶点属性
+    glEnableVertexAttribArray(0);
+}
+
+//矩形由2个三角形组成，需要6个点。但是有两个点重复了，浪费存储空间
+//float rectangleVertices[] = {
+//        // 第一个三角形
+//        0.5f, 0.5f, 0.0f,   // 右上角
+//        0.5f, -0.5f, 0.0f,  // 右下角
+//        -0.5f, 0.5f, 0.0f,  // 左上角
+//        // 第二个三角形
+//        0.5f, -0.5f, 0.0f,  // 右下角
+//        -0.5f, -0.5f, 0.0f, // 左下角
+//        -0.5f, 0.5f, 0.0f   // 左上角
+//};
+
+void generateEBO() {
+    glGenBuffers(1, &EBO);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+}
+
+void drawTriangle() {
+    glUseProgram(mProgram);
+
+//    glDrawArrays(GL_TRIANGLES, 0, 3);
+//    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    glBindVertexArray(VAO);
+    glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+    glBindVertexArray(0);
+//    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
+}
+
+
+//以上绑定了一个三角形，但是如果我们有几十几百个物体，这样绑定和配置就太麻烦了
+//使用顶点数组对象VAO Vertex Array Object，可以保存所有的VBO对象和配置
+//利用VAO可以同时存储VBO和EBO
+uint generateVAO() {
+    uint vao = 0;
+    //生成VAO
+    glGenVertexArrays(1, &vao);
+
+    //绑定VAO
+    glBindVertexArray(vao);
+    //生成VAO
+    generateVBO();
+    //告诉OpenGL如何解析VBO并启动VBO
+    handleVBO();
+
+    //生成绑定EBO
+    generateEBO();
+
+    //解绑VAO
+    glBindVertexArray(0);
+    return vao;
 }
 
 
@@ -142,13 +255,26 @@ void onSurfaceChanged(int width, int height) {
 
 void onSurfaceCreated() {
     LOGD("OpenGL", "onSurfaceCreated");
-    generateVBO();
-    linkProgram();
+    //将数据灌入显存
+//    generateVBO();
+//    generateEBO();
+    VAO = generateVAO();
 
+    //链接我们自定义的着色器
+    mProgram = linkProgram();
+    if (mProgram == 0) {
+        LOGE("opengl", "onSurfaceCreated link fail");
+        return;
+    }
+
+    handleVBO();
+    //OpenGL ES里剔除了这个方法
+//    glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
 }
 
 void onDrawFrame() {
     glClearColor(0.2, 0.3, 0.3, 1.0);
     glClear(GL_COLOR_BUFFER_BIT);
     LOGD("OpenGL", "onDrawFrame");
+    drawTriangle();
 }
